b9092a46bd03bdbb6dffff2ebf9af8243fbebfc4
Nirajan Mahara <np03a190125@heraldcollege.edu.np>
2021-05-11 21:46:58 +0545

    ADD starter files for components (Cards, Chart, CountryPicker)...🚀

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+// importing each components here === ==>
+export { default as Cards } from "./Cards/Cards";
+export { default as Chart } from "./Chart/Chart";
+export { default as CountryPicker } from "./CountryPicker/CountryPicker";
