79b3ebb93e02f2f532cc9779f7c0f71e26c1f205
Nirajan Mahara <np03a190125@heraldcollege.edu.np>
2021-05-11 22:53:11 +0545

    ADD Cards with functionality & Styles🚀

--- src/App.js
+++ src/App.js
@@ -13,17 +13,24 @@ import { fetchData } from './api';
 
 
 class App extends React.Component {
+  state = {
+    data: {},
+  }
 
   async componentDidMount() {
     const fetchedData = await fetchData();
 
     console.log(fetchedData);
+
+    this.setState({ data: fetchedData});
   }
 
   render() {
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
