db3ea51a5185b1bc18d1d6ffd33879beba05643b
Nirajan Mahara <np03a190125@heraldcollege.edu.np>
2021-05-12 23:58:05 +0545

    ADD barChart with dynamic functionality🚀

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,7 +5,7 @@ import { Line, Bar } from 'react-chartjs-2';
 
 import styles from './Chart.module.css';
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     const [ dailyData, setDailyData ] = useState([]);
 
     useEffect(() => {
@@ -16,7 +16,7 @@ const Chart = () => {
         // console.log(dailyData);
 
         fetchAPI();
-    }, [ setDailyData ]);
+    }, []);
 
     const lineChart = (
         dailyData.length !== 0
@@ -40,9 +40,35 @@ const Chart = () => {
             />) : null
     );
 
+    console.log( confirmed, recovered, deaths );
+
+    const barChart = (
+        confirmed 
+        ? (
+            <Bar 
+                data={{
+                    labels: ['Infected', 'Recovered', 'Deaths'],
+                    datasets: [{
+                        label: 'People',
+                        backgroundColor: [
+                            'rgba(0, 0, 255, 0.75)',
+                            'rgba(0, 255, 0, 0.75)',
+                            'rgba(255, 0, 0, 0.75)',
+                        ],
+                        data: [ confirmed.value, recovered.value, deaths.value ]
+                    }]
+                }}
+                options={{
+                    legend: { display: false },
+                    title: { display: true, text: `Current state in ${country}`},
+                }}
+            />
+        ) : null
+    )
+
     return (
         <div className={ styles.container }>
-            { lineChart }
+            {country ? barChart : lineChart }
         </div>
     )
 }
